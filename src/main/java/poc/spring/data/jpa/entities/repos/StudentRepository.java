6d13693ac7f03a9e89febbd716c213ebbccb0396
kaushlendra <kaushlendra.chauhan@xoriant.com>
2020-03-05 14:00:05 +0530

    kept incorrect and correct @Query for reference

--- src/main/java/poc/spring/data/jpa/entities/repos/StudentRepository.java
+++ src/main/java/poc/spring/data/jpa/entities/repos/StudentRepository.java
@@ -8,6 +8,10 @@ import poc.spring.data.jpa.entities.Student;
 
 public interface StudentRepository extends CrudRepository<Student, Long>{
 
+	// [PROBLEM]
+	// @Query("SELECT s FROM Student s WHERE s.adresses.id = :aId")
+	
+	// [SOLUTION]
 	@Query("SELECT s FROM Student s INNER JOIN s.adresses sa WHERE sa.id = :aId")
 	Student findByAddressId(@Param("aId")Long addressId); 
 }
